ad7841423ab208513aaa6ccc4f29d4d019b72e01
Spencer Mefford <spencermefford@gmail.com>
2018-03-06 11:19:58 -0700

    Linting

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,18 @@
 import React from 'react';
-import { hot } from 'react-hot-loader'
+import { hot } from 'react-hot-loader';
 import { Switch, BrowserRouter as Router, Route } from 'react-router-dom';
 import './Main.css';
 
-import TodoList from "./TodoList";
+import TodoList from './TodoList';
 
-const App = () => {
-  return (
-    <Router>
-      <div>
-        <Switch>
-          <Route component={TodoList} />
-        </Switch>
-      </div>
-    </Router>
-  );
-};
+const App = () => (
+  <Router>
+    <div>
+      <Switch>
+        <Route component={TodoList} />
+      </Switch>
+    </div>
+  </Router>
+);
 
 export default hot(module)(App);
