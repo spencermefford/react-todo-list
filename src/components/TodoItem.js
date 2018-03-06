012cf8f0241b6446fa399daceeff16197c2ab598
Spencer Mefford <spencermefford@gmail.com>
2018-03-06 09:58:39 -0700

    One way data flow finally working

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,17 +1,22 @@
 import React from 'react';
+import { Map } from 'immutable';
 
 class TodoItem extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return (
       <li>
+        <input type="checkbox" name="complete" checked={this.props.item.isComplete} onChange={this.handleChange} />
         {this.props.item.title}
+        - {String(this.props.item.isComplete)}
       </li>
     )
   }
+
+  handleChange = () => {
+    const changed = Map(this.props.item).set('isComplete', !this.props.item.isComplete).toObject();
+
+    this.props.onChange(changed);
+  };
 }
 
 export default TodoItem;
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,12 +2,14 @@ import React from 'react';
 import TodoInput from "./TodoInput";
 import TodoItem from "./TodoItem";
 import TodoActions from "./TodoActions";
-import {todoList, mainTitle} from './Main.css';
+import { todoList, mainTitle } from './Main.css';
+import { List } from 'immutable';
 
 class TodoList extends React.Component {
   state = {
     todoItems: [],
   };
+
 
   render() {
     const todoItems = this.state.todoItems.map((item) => {
@@ -33,14 +35,18 @@ class TodoList extends React.Component {
   handleInputChange = (value) => {
     const item = {
       id: new Date().getTime(),
-      title: value
+      title: value,
+      isComplete: false,
     };
 
     this.setState({ todoItems: [...this.state.todoItems, item] });
   };
 
   handleItemChange = (item) => {
-    console.log('item', item);
+    const todoItems = List(this.state.todoItems);
+    const index = todoItems.findIndex(i => i.id === item.id);
+
+    this.setState({ todoItems: todoItems.set(index, item).toArray() });
   };
 }
 
